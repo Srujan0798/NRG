990a1ededfa5e8bfdc890c540b2ad2864c194fb5
srujan sai <srujansai@MacBook-Air.local>
2026-04-21 01:05:51 +0530

    ci: update GitHub Actions workflow, Makefile, docker-compose, and linting configs

---
+++ frontend/jest.config.js
@@ -0,0 +1,14 @@
+/** @type {import('ts-jest').JestConfigWithTsJest} */
+module.exports = {
+  preset: 'ts-jest',
+  testEnvironment: 'jsdom',
+  roots: ['<rootDir>/src', '<rootDir>/tests'],
+  testPathIgnorePatterns: [
+    '/node_modules/',
+    '/e2e/',
+    '/dist/',
+  ],
+  moduleNameMapper: {
+    '\\.(css|less|scss|sass)$': 'identity-obj-proxy',
+  },
+};
